4877ae7c9d3cac6a0bd322b1cd9a3062bedb397d
“Guillaume <guillaume.ehret.java@gmail.com>
2014-08-29 18:30:21 +0200

    ajout tp

---
+++ app/scripts/controllers/conference.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('myconfs').controller('ConfCtrl',['$scope', function($scope){
+
+}]);
---
+++ app/scripts/data/data.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var conferences = [
+    {
+        id: 1,
+        name: 'Mix-IT',
+        description: 'Des conférences pour faire des découvertes, des jeux pour apprendre, des ateliers de développement ou créatifs, Mix-IT c\'est 2 jours intenses de progrès et de rencontres ! La soirée, ouverte à tous, est aussi le moment idéal pour faire des connaissances et s\'ouvrir aux autres cultures.',
+        place: 'Lyon',
+        country: 'France',
+        startdate: '2014-04-29T08:00:00.000',
+        enddate: '2014-04-30T18:00:00.000',
+        participant: '500',
+        price: '50',
+        image: 'logo-mixit.png',
+        themes: [1, 3, 5, 12]
+    },
+    {
+        id: 2,
+        name: 'Mix-IT',
+        description: '',
+        place: '',
+        country: '',
+        startdate: '',
+        enddate: '',
+        participant: '',
+        price: '',
+        image: '',
+        themes: []
+    },
+    {
+        id: 3,
+        name: 'Mix-IT',
+        description: '',
+        place: '',
+        country: '',
+        startdate: '',
+        enddate: '',
+        participant: '',
+        price: '',
+        image: '',
+        themes: []
+    },
+    {
+        id: 4,
+        name: 'Mix-IT',
+        description: '',
+        place: '',
+        country: '',
+        startdate: '',
+        enddate: '',
+        participant: '',
+        price: '',
+        image: '',
+        themes: []
+    }
+];
+
+var themes = [
+    { id: 1, label: 'Java'},
+    { id: 2, label: 'Angular JS'},
+    { id: 3, label: 'Agilite'},
+    { id: 4, label: 'Android'},
+    { id: 5, label: 'Mobilité'},
+    { id: 6, label: 'Javascript'},
+    { id: 7, label: 'HTML 5'},
+    { id: 8, label: 'CSS'},
+    { id: 9, label: '.NET'},
+    { id: 10, label: 'Cloud'},
+    { id: 11, label: 'Test'},
+    { id: 12, label: 'Web'}
+
+];
